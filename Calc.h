0fd3cd25dbcf9f017efcc15b31e671242499bd10
kouki2000 <kouki-tus@outlook.jp>
2021-01-07 18:27:07 +0900

    約数判定

--- Calc.h
+++ Calc.h
@@ -1,7 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
 class Calc
 {
 public:
     int is_prime(int x);
     int permutation(int left, int right);
     int combination(int left, int right);
+    vector<int> enum_divisors(int num);
 };
--- sample.cpp
+++ sample.cpp
@@ -11,14 +11,10 @@ int main()
 {
     Calc cl;
     Sort st;
-    int a[] = {1, 3, 2};
-    // cout << cl.is_prime(11) << endl;
-    // cout << cl.combination(7, 3) << endl;
-    // st.bubble(a, 3);
-    // rep(i, 3)
-    // {
-    //     cout << a[i] << endl;
-    // }
-    st.bit(3);
+    vector<int> ans = cl.enum_divisors(120);
+    rep(i, ans.size())
+    {
+        cout << ans[i] << endl;
+    }
     return 0;
 }
