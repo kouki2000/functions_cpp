fbb4521f88df8749bec95a83725cf23e320f4e37
kouki2000 <kouki-tus@outlook.jp>
2021-01-07 18:41:22 +0900

    約数

--- Calc.h
+++ Calc.h
@@ -6,9 +6,8 @@ public:
     int is_prime(int x);
     int permutation(int left, int right);
     int combination(int left, int right);
-<<<<<<< HEAD
+
     vector<int> enum_divisors(int num);
-=======
+
     int cumulative_sum(int n, int k, int *a);
->>>>>>> cumulative_sum
 };
